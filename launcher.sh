d0358426454ab530a205dc3076129ab051f91887
nicktoper <ntoper@manycore.io>
2024-12-16 20:58:46 -0800

    init commit

---
+++ launcher.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Print JVM version and a separator line
+java -version
+echo "-----"
+
+# Iterate through levels 0 to 4
+for level in 0 1 2 3 4; do
+    echo "JVM optimization blocked at $level"
+    java -XX:TieredStopAtLevel=$level -jar intractperf-1.0-SNAPSHOT-jar-with-dependencies.jar
+done
+
